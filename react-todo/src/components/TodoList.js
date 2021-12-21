62ff38320021612282683c5bc00100d3a4a4668d
YLPeter <q510939123@gmail.com>
2021-12-21 09:27:08 +0000

    trying get data

--- react-todo/src/components/TodoList.js
+++ react-todo/src/components/TodoList.js
@@ -1,9 +1,30 @@
 import React,{useState} from 'react'
 import TodoForm from './TodoForm'
 import Todo from './Todo' 
+const axios = require('axios');
 function TodoList() {
     const [todos,setTodos] = useState([])
+    const [externalServerResponse, setExternalServerResponse] = useState(
+        "no response"
+      );
     const findIfExistNum = todoId => [...todos].find(todo => todo.id === todoId)
+
+    const getExternalAPI = async () => {
+        console.log("calling external api from client");
+        const result = await fetch("/api/tasks");
+        console.log("result", result);
+        const data = await result.json();
+        console.log("data", data);
+        setExternalServerResponse(JSON.stringify(data[0]));
+      };
+    const getAlltasks = async (callbackfunction) => await axios({
+        method: 'get',
+        url: "/api/tasks",
+    }).then((res)=>{
+            callbackfunction(res)
+        }
+    );
+
     const valuetodo = todo => {
         if(!todo.text || /^\s*$/.test(todo.text)){
             console.log("not empty")
@@ -30,6 +51,8 @@ function TodoList() {
         const newTodos = [todo,...todos];
         setTodos(newTodos);
         console.log(...todos);
+        getExternalAPI()
+        console.log("getAlltasks", getAlltasks())
     }
 
     const updateTodo = (todoId,newValue) => {
