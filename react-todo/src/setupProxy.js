0639a7d2a7356ba48a73e5123910c3fbb68913f9
YLPeter <q510939123@gmail.com>
2021-12-23 06:51:57 +0000

    change to heroku package

--- react-todo/src/setupProxy.js
+++ react-todo/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  let aws = false
+  let aws = true
   let address = 'https://sheltered-island-26661.herokuapp.com'
   if (aws){
     address = 'http://3.135.77.77:4000'
